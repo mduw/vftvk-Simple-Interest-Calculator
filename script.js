7fd3709dd1a0d700e185cb418d67dc18482f75ba
mitchelldang <mlcdang@uw.edu>
2021-06-18 20:32:59 +0700

    add logic

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+function getCurrentRate() {
+    rate = document.getElementById("rate").value;
+    document.getElementById("currentRate").innerHTML = rate;
+}
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    rate = document.getElementById("rate").value;
+    year = document.getElementById("years").value;
+    document.getElementById("currentRate").innerHTML=rate;
+    interest = p * rate * year;
+    resultElement = document.getElementById("result");
+    resultElement.innerHTML = interest;
 }
         
