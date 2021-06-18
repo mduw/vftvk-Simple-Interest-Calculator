b405352e010278837ed4f255100f2c17cdd56a5c
mitchelldang <mlcdang@uw.edu>
2021-06-18 22:27:07 +0700

    update error handling

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function getCurrentRate() {
-  rate = document.getElementById("rate").value || 0;
+  rate = document.getElementById("rate").value || 1;
   document.getElementById("currentRate").innerHTML = rate;
+  return rate;
 }
 
 function compute() {
-  p = document.getElementById("principal").value || 0;
-  getCurrentRate();
+  p = document.getElementById("principal").value;
+  if (p <= 0) {
+      alert('Please enter a positive principal');
+      document.getElementById("principal").focus();
+  }
+  rate = getCurrentRate();
   year = document.getElementById("years").value || 0;
 
   interest = p * rate/100 * year;
