1cbc52485af2048d96297ddbf2e4fb0d47e012fa
mitchelldang <mlcdang@uw.edu>
2021-06-18 21:22:09 +0700

    update calculator

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function getCurrentRate() {
-    rate = document.getElementById("rate").value;
-    document.getElementById("currentRate").innerHTML = rate;
+  rate = document.getElementById("rate").value || 0;
+  document.getElementById("currentRate").innerHTML = rate;
 }
-function compute()
-{
-    p = document.getElementById("principal").value;
-    rate = document.getElementById("rate").value;
-    year = document.getElementById("years").value;
-    document.getElementById("currentRate").innerHTML=rate;
-    interest = p * rate * year;
-    resultElement = document.getElementById("result");
-    resultElement.innerHTML = interest;
+function compute() {
+  p = document.getElementById("principal").value || 0;
+  getCurrentRate();
+  year = document.getElementById("years").value || 1;
+  interest = p * rate * year;
+  nextDue = new Date().getFullYear() + parseInt(year);
+  document.getElementById("oAmount").innerHTML = p;
+  document.getElementById("oRate").innerHTML = rate;
+  document.getElementById("oNextDue").innerHTML = nextDue;
+  document.getElementById("oInterest").innerHTML = interest;
 }
-        
