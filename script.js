854ce8a13e435fcf70acf2c6d52aa52142acc30b
mitchelldang <mlcdang@uw.edu>
2021-06-18 21:32:08 +0700

    update

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ function getCurrentRate() {
   rate = document.getElementById("rate").value || 0;
   document.getElementById("currentRate").innerHTML = rate;
 }
+
 function compute() {
   p = document.getElementById("principal").value || 0;
   getCurrentRate();
-  year = document.getElementById("years").value || 1;
+  year = document.getElementById("years").value || 0;
+
   interest = p * rate * year;
   nextDue = new Date().getFullYear() + parseInt(year);
+
+  document.getElementById("result").style.display = "block";
   document.getElementById("oAmount").innerHTML = p;
   document.getElementById("oRate").innerHTML = rate;
   document.getElementById("oNextDue").innerHTML = nextDue;
